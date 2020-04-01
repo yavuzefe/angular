1087cc2aeb8b5967358f84d2da58030933653318
yavuzefe <yavuzefe@gmail.com>
2020-04-01 11:29:16 +0300

    Angular RxJs Introduction

--- angularEfe/angularChocoEfe/src/app/dishdetail/dishdetail.component.ts
+++ angularEfe/angularChocoEfe/src/app/dishdetail/dishdetail.component.ts
@@ -19,7 +19,7 @@ export class DishdetailComponent implements OnInit {
     private location: Location) { }
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
-    this.dish = this.dishservice.getDish(id);
+    this.dishservice.getDish(id).subscribe(dish => this.dish = dish);
  }
   goBack(): void {
     this.location.back();
--- angularEfe/angularChocoEfe/src/app/services/dish.service.ts
+++ angularEfe/angularChocoEfe/src/app/services/dish.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { Observable } from 'rxjs';
+// import 'rxjs/add/operators/delay';
+// import 'rxjs/add/observable/of';
+// import 'rxjs-compat/add/observable/of';
+
+import { of as ObservableOf } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable()
 export class DishService {
 
   constructor() { }
 
-  getDishes(): Dish[] {
-    return DISHES;
+  getDishes(): Observable< Dish[]> {
+    return ObservableOf(DISHES).pipe(delay(2000));
   }
 
-  getDish(id: number): Dish {
-    return DISHES.filter((dish) => (dish.id === id))[0];
+  getDish(id: number): Observable<Dish> {
+    return ObservableOf(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000));
   }
 }
